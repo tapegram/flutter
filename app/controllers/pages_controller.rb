7629fffe5f82ca64f7409e63d9f07cbdb241b957
Tavish <tapegram@gmail.com>
2016-08-21 15:04:07 +0000

    Created Pages controller. Added explorem home, index, and profile pages. Changed root URL to index.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+# The pages controller contains all of the ode for any page inside of /pages
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def profile
+  end
+
+  def explore
+  end
+end
